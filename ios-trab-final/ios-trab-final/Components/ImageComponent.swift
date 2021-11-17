6576176a398d1c652a45372c76b786e409532805
tazmonia <alaenedesousa@gmail.com>
2021-11-16 22:00:35 -0300

    more updates heheheh
    fiz mais coisa USHAUHSUAHSUAHS

--- ios-trab-final/ios-trab-final/Components/ImageComponent.swift
+++ ios-trab-final/ios-trab-final/Components/ImageComponent.swift
@@ -45,17 +45,39 @@ class ImageComponent: UIView {
     private func setupConstraints() {
         imageComponent
             .widthAnchor
-            .constraint(equalToConstant: imageProps.isDetailsPage ? ImageConstants.extraLarge : ImageConstants.medium)
+            .constraint(equalToConstant: imageProps.isDetailsPage ? ImageConstants.extraLarge : ImageConstants.extraMedium)
             .isActive = true
+        
         imageComponent
             .heightAnchor
             .constraint(equalToConstant: imageProps.isDetailsPage ? ImageConstants.bigger :  ImageConstants.large)
+            .isActive = true
+        
+        imageComponent
+            .topAnchor
+            .constraint(equalTo: topAnchor, constant: ImageConstants.medium)
+            .isActive = true
+        
+        imageComponent
+            .bottomAnchor
+            .constraint(equalTo: bottomAnchor, constant: ImageConstants.medium)
+            .isActive = true
+        
+        imageComponent
+            .leftAnchor
+            .constraint(equalTo: leftAnchor, constant: ImageConstants.medium)
+            .isActive = true
+        
+        imageComponent
+            .rightAnchor
+            .constraint(equalTo: rightAnchor, constant: ImageConstants.medium)
             .isActive = true
     }
     
     // MARK: - Constants
     private enum ImageConstants {
-        static let medium = CGFloat(24.0)
+        static let medium = CGFloat(16.0)
+        static let extraMedium = CGFloat(24.0)
         static let large = CGFloat(32.0)
         static let extraLarge = CGFloat(48.0)
         static let bigger = CGFloat(64.0)
