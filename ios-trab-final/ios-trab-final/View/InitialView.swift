1fcec93a798bae57aa201971d1a374395e8d6fa4
Alaene Sousa <Alaene.Sousa@ab-inbev.com>
2021-11-27 12:28:30 -0300

    Create details without Main.Storyboard

--- ios-trab-final/ios-trab-final/View/InitialView.swift
+++ ios-trab-final/ios-trab-final/View/InitialView.swift
@@ -13,4 +13,49 @@ class InitialView: UIView {
     lazy var movieDescription = String()
     lazy var movieCollectionView = UICollectionView()
     
+    lazy var stackView: UIStackView = {
+        let sv = UIStackView()
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.axis = .vertical
+        sv.distribution = .fillEqually
+        sv.addArrangedSubview(plainViewControllerButton)
+        sv.addArrangedSubview(subclassedViewButton)
+        return sv
+    }()
+    
+    lazy var plainViewControllerButton: UIButton = {
+       let bt = UIButton()
+        bt.setTitle("Plain ViewController", for: .normal)
+        bt.addTarget(self, action: #selector(goPlain), for: .touchDown)
+        return bt
+    }()
+    
+    lazy var subclassedViewButton: UIButton = {
+        let bt = UIButton()
+        bt.setTitle("Subclassed ViewController", for: .normal)
+        bt.addTarget(self, action: #selector(goSubclass), for: .touchDown)
+        return bt
+    }()
+    
+    @objc func goPlain() {
+//        self.navigationController?.pushViewController(DetaillViewController(), animated: true)
+    }
+    
+    @objc func goSubclass() {
+//        self.navigationController?.pushViewController(DetaillViewController(), animated: true)
+    }
+
+    func addConstraints() {
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.topAnchor),
+            stackView.bottomAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.bottomAnchor),
+            stackView.leadingAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.leadingAnchor),
+            stackView.trailingAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+    
 }
--- ios-trab-final/ios-trab-final/ViewController/InitialViewController.swift
+++ ios-trab-final/ios-trab-final/ViewController/InitialViewController.swift
@@ -9,5 +9,9 @@ import Foundation
 import UIKit
 
 class InitialViewController: UIViewController {
-    
+    override func loadView() {
+        let view = UIView()
+        self.view = view
+        self.view.backgroundColor = .blue
+    }
 }
